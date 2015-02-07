e468875c78cdaa0d1fa307f1a7a468b46f0366ac
Marin Abernethy <maabernethy@gmail.com>
2015-02-07 15:27:25 -0500

    Add year 2014 namespacing
    * Move files into respective 2014 directories
    * Correct file paths to include 2014

--- app.rb
+++ app.rb
@@ -17,46 +17,4 @@ configure do
   set :sass, Compass.sass_engine_options
 end
 
-get '/' do
-  haml :index
-end
-
-get '/schedule' do
-  haml :schedule
-end
-
-get '/robin-ward' do
-  haml :robin_ward
-end
-
-get '/joe-fiorini' do
-  haml :joe_fiorini
-end
-
-get '/erik-bryn' do
-  haml :erik_bryn
-end
-
-get '/luke-melia' do
-  haml :luke_melia
-end
-
-get '/robert-jackson' do
-  haml :robert_jackson
-end
-
-get '/dan-gebhardt' do
-  haml :dan_gebhardt
-end
-
-get '/katie-gengler' do
-  haml :katie_gengler
-end
-
-get '/phil-renaud' do
-  haml :phil_renaud
-end
-
-get '/edward-faulkner' do
-  haml :edward_faulkner
-end
+require_relative 'routes/init'
---
+++ routes/2014/routes.rb
@@ -0,0 +1,43 @@
+get '/2014' do
+  haml '2014/index'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/schedule' do
+  haml '2014/schedule'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/robin-ward' do
+  haml '2014/robin_ward'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/joe-fiorini' do
+  haml '2014/joe_fiorini'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/erik-bryn' do
+  haml '2014/erik_bryn'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/luke-melia' do
+  haml '2014/luke_melia'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/robert-jackson' do
+  haml '2014/robert_jackson'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/dan-gebhardt' do
+  haml '2014/dan_gebhardt'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/katie-gengler' do
+  haml '2014/katie_gengler'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/phil-renaud' do
+  haml '2014/phil_renaud'.to_sym, layout: '2014/layout'.to_sym
+end
+
+get '/2014/edward-faulkner' do
+  haml '2014/edward_faulkner'.to_sym, layout: '2014/layout'.to_sym
+end
