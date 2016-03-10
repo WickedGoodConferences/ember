12f939169aa4fdca8db1ce4b9b1e7e1c6a1e6ea1
Brian Cardarella <brian@dockyard.com>
2016-03-10 13:00:35 -0500

    let's encrypt setup

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require 'compass'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
+# before do
+  # redirect request.url.sub('http', 'https') unless request.secure?
+# end
+
 configure do
   Compass.configuration do |config|
     config.project_path = File.dirname(__FILE__)
@@ -15,6 +19,10 @@ configure do
 
   set :haml, { :format => :html5 }
   set :sass, Compass.sass_engine_options
+end
+
+get "/.well-known/acme-challenge/:id" do
+  "74BAtyhm34DVSSTdorjWbYw4CzdrAXFA5DG5MnZgLUs.hnBafX12zBaGCgOrKNBmq4td_5N3g4XHpA-Vo-BWrhs"
 end
 
 require_relative 'routes/init'
