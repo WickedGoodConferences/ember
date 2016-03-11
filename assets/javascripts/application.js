5233ad7bb456033178f1d89fe179a2f571dc6d4c
Brian Cardarella <brian@dockyard.com>
2016-03-11 04:21:13 -0500

    Randomize word

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,8 @@
 #= require jquery.min.js
 #= require easeScroll.js
 #= require mobileNav.js
+
+$(function() {
+  var words = ['Emberists', 'Emberiños', 'Embereñas'];
+  $('.people-who-do-ember').text(words[Math.floor(Math.random()*words.length)]);
+});
